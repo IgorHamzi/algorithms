07534021e4110a0f16b18431e4847e76f2eb14b0
IgorHamzi <igorwhamzi@gmail.com>
2023-01-04 18:52:12 -0300

    style: fix flake8

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,10 +1,10 @@
 def quickSort(arr):
 
     elements = len(arr)
-    
+
     if elements < 2:
         return arr
-    
+
     current_position = 0
 
     for i in range(1, elements):
@@ -17,17 +17,17 @@ def quickSort(arr):
     temp = arr[0]
     arr[0] = arr[current_position]
     arr[current_position] = temp
-    
+
     left = quickSort(arr[0:current_position])
     right = quickSort(arr[current_position+1:elements])
 
     arr = left + [arr[current_position]] + right
-    
+
     return arr
 
 
 def is_anagram(first_string, second_string):
-    
+
     sorted_str1 = quickSort(list(first_string.lower()))
     sorted_str2 = quickSort(list(second_string.lower()))
 
