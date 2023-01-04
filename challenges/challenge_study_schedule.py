86cb41b18adb11497f2712f03037e6afde192b2b
IgorHamzi <igorwhamzi@gmail.com>
2023-01-04 14:23:53 -0300

    feat: add challege palindromes recurive logic

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,21 +1,14 @@
-def study_schedule(permanence_period, target_time):
-    counter = 0
-
-    if target_time is None:
-        return None
-    else:
-        try:
-            for i in permanence_period:
-                for j in range(i[0], i[1]+1):
-                    if j == target_time: 
-                        counter += 1
-            return counter
-        except:
-            return None
-
-
-
-# if __name__ == '__main__':
-#    permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-#    target_time = 5  # saída: 3, pois a quarta, a quinta e a sexta pessoa estudante ainda estavam estudando nesse horário.
-#    study_schedule(permanence_period, target_time)
+#def study_schedule(permanence_period, target_time):
+#    students = 0
+#
+#    if target_time and permanence_period is None or type(permanence_period) != list:
+#        return None
+#    else:
+#        for i in permanence_period:
+#            if i is None: return None
+#            if i[0] is None or i[1] is None: return None
+#            if type(i[0]) != int or type(i[1]) != int: return None
+#            for j in range(i[0], i[1]+1):
+#                if j == target_time:
+#                    students += 1
+#        return students
