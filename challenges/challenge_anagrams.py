e6cd8a3ec6589bcdf73717e235b4ccaa84b6621d
IgorHamzi <igorwhamzi@gmail.com>
2023-01-04 18:49:07 -0300

    refactor: challenge anagrams logic (quick sort)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,27 +1,39 @@
-def partition(array, low, high):
-    pivot = array[high]
-    i = low - 1
+def quickSort(arr):
 
-    for j in range(low, high):
-        if array[j] <= pivot:
-            i = i + 1
-            (array[i], array[j]) = (array[j], array[i])
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    elements = len(arr)
+    
+    if elements < 2:
+        return arr
+    
+    current_position = 0
 
-    return i + 1
+    for i in range(1, elements):
+         if arr[i] <= arr[0]:
+              current_position += 1
+              temp = arr[i]
+              arr[i] = arr[current_position]
+              arr[current_position] = temp
 
+    temp = arr[0]
+    arr[0] = arr[current_position]
+    arr[current_position] = temp
+    
+    left = quickSort(arr[0:current_position])
+    right = quickSort(arr[current_position+1:elements])
 
-def quickSort(array, low, high):
-    if low < high:
-        partition_index = partition(array, low, high)
-        quickSort(array, low, partition_index - 1)
-        quickSort(array, partition_index + 1, high)
+    arr = left + [arr[current_position]] + right
+    
+    return arr
 
 
 def is_anagram(first_string, second_string):
-    sorted_str1 = quickSort(first_string, 0, len(first_string) - 1)
-    sorted_str2 = quickSort(second_string, 0, len(second_string) - 1)
+    
+    sorted_str1 = quickSort(list(first_string.lower()))
+    sorted_str2 = quickSort(list(second_string.lower()))
 
     anagram = sorted_str1 == sorted_str2
 
-    return (sorted_str1, sorted_str2, anagram)
+    if first_string == '' or second_string == '':
+        anagram = False
+
+    return (''.join(sorted_str1), ''.join(sorted_str2), anagram)
