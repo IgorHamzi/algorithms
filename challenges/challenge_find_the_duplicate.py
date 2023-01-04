a93995eb0b48b460bfa2c95e0b185871f1d61959
IgorHamzi <igorwhamzi@gmail.com>
2023-01-04 15:54:15 -0300

    feat: add challenge find duplicate logic

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,18 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    if len(nums) <= 1:
+        return False
+
+    nums.sort()
+
+    j = 0
+
+    for i in nums:
+        if type(i) != int or i < 0:
+            return False
+        elif i != j:
+            j = i
+        else:
+            return i
+
+    return False
