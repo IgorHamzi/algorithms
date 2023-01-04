a788b1882410eff2fe2a4aab85138be14d4a3bf4
IgorHamzi <igorwhamzi@gmail.com>
2023-01-04 16:27:58 -0300

    feat: add challenge anagrams logic

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,27 @@
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+    return i + 1
+
+
+def quickSort(array, low, high):
+    if low < high:
+        partition_index = partition(array, low, high)
+        quickSort(array, low, partition_index - 1)
+        quickSort(array, partition_index + 1, high)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    sorted_str1 = quickSort(first_string, 0, len(first_string) - 1)
+    sorted_str2 = quickSort(second_string, 0, len(second_string) - 1)
+
+    anagram = sorted_str1 == sorted_str2
+
+    return (sorted_str1, sorted_str2, anagram)
