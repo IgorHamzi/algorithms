855da38ec9eb0fe26206e23e934dcc833e3a1994
IgorHamzi <igorwhamzi@gmail.com>
2023-01-04 19:34:02 -0300

    style: fix flake8

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,15 +1,13 @@
 def study_schedule(permanence_period, target_time):
     students = 0
 
-    if target_time == None or type(permanence_period) != list:
-         return None
-    else:
-        for i in permanence_period:
-            if i is None: return None
-            if i[0] is None or i[1] is None: return None
-            if type(i[0]) != int or type(i[1]) != int: return None
-            for j in range(i[0], i[1]+1):
-                if j == target_time:
-                    students += 1
-        
+    if target_time is None or type(permanence_period) != list:
+        return None
+
+    for i in permanence_period:
+        if type(i[0]) != int or type(i[1]) != int:
+            return None
+        if str(target_time) >= str(i[0]) and str(target_time) <= str(i[1]):
+                students += 1
+
     return students
