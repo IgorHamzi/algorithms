285699b535fa716efd5d4089762a21541c9a2096
IgorHamzi <igorwhamzi@gmail.com>
2023-01-04 14:23:28 -0300

    feat: add challenge palidromes iterative logic

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,17 @@
 def is_palindrome_iterative(word):
-    raise NotImplementedError
+    index = 0
+
+    if word == '':
+        return False
+
+    for letter in reversed(word):
+        if letter != word[index]:
+            return False
+        else:
+            index += 1
+
+    return True
+
+
+if __name__ == '__main__':
+    is_palindrome_iterative('arara')
