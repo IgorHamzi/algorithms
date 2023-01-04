5d75468ec2091fac8ef5ed76f1387d6e98d85366
IgorHamzi <igorwhamzi@gmail.com>
2023-01-04 16:35:14 -0300

    test: add test_encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 def test_encrypt_message():
-    pass
+    invalid_key1 = encrypt_message("ABCD", 20)
+    assert invalid_key1 == "DCBA"
+
+    invalid_key2 = encrypt_message("ABBCCA", 4)
+    assert invalid_key2 == "AC_CBBA"
+
+    invalid_key3 = encrypt_message("ABCD", 2)
+    assert invalid_key3 == "DC_BA"
+
+    with pytest.raises(TypeError):
+        encrypt_message("AABBCC", None)
+    
+    with pytest.raises(TypeError):
+        encrypt_message(None, 3)
