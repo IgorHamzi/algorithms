1672efac39ebc73ff5cb2c9c3eec1c4c272e9aa8
IgorHamzi <igorwhamzi@gmail.com>
2023-01-04 19:11:08 -0300

    refactor: study schedule logic

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,17 +1,15 @@
-# def study_schedule(permanence_period, target_time):
-#     students = 0
-#
-#     permanence_period = permanence_period[0]
-#
-#     if target_time and permanence_period
-# is None or type(permanence_period) != list:
-#          return None
-#     else:
-#         for i in permanence_period:
-#             if i is None: return None
-#             if i[0] is None or i[1] is None: return None
-#             if type(i[0]) != int or type(i[1]) != int: return None
-#             for j in range(i[0], i[1]+1):
-#                 if j == target_time:
-#                     students += 1
-#         return students
+def study_schedule(permanence_period, target_time):
+    students = 0
+
+    if target_time == None or type(permanence_period) != list:
+         return None
+    else:
+        for i in permanence_period:
+            if i is None: return None
+            if i[0] is None or i[1] is None: return None
+            if type(i[0]) != int or type(i[1]) != int: return None
+            for j in range(i[0], i[1]+1):
+                if j == target_time:
+                    students += 1
+        
+    return students
